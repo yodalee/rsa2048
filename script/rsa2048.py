b474ea3a8d88a5de05750e6254cdc9c5eca7a992
Yoda Lee <lc85301@gmail.com>
2024-04-06 01:21:00 +0800

    add implementation of chunk/dechunk

--- script/rsa2048.py
+++ script/rsa2048.py
@@ -1,3 +1,4 @@
+import typing
 
 def intmul_2048(a, b, n):
     """Calculate a * b % n with a, b, n are 2048 bits integer"""
@@ -6,3 +7,20 @@ def intmul_2048(a, b, n):
     assert(n < 2**2048)
     return (a * b) % n
 
+def chunk(a: int) -> list[int]:
+    """For 32 bits operation l = 11, chunk number every 11 bits;
+       List [0] will store the LSB
+       RSA2048 and RSA4096 will create poly size of 384 and 768.
+    """
+    xs = [None] * 384
+    for i in range(384):
+        xs[i] = a & 0x7FF
+        a >>= 11
+    return xs
+
+def dechunk(xs: list[int]) -> int:
+    a = 0
+    for x in reversed(xs):
+        a <<= 11
+        a = a | x
+    return a
--- script/test_rsa2048.py
+++ script/test_rsa2048.py
@@ -3,7 +3,7 @@ import random
 from rsa2048 import *
 
 class TestIntmul2048(unittest.TestCase):
-    def test_intmul_2048_result_equals_1(self):
+    def test_intmul_2048(self):
         # Generate random 2048-bit integers a, b, and n
         a = random.getrandbits(2048)
         b = random.getrandbits(2048)
@@ -18,6 +18,11 @@ class TestIntmul2048(unittest.TestCase):
 
         # Check if the result is equal to 1
         self.assertEqual(result, (a * b) % n, "Result of intmul_2048 is incorrect")
+
+    def test_chunk_dechunk(self):
+        a = random.getrandbits(2048)
+        b = dechunk(chunk(a))
+        self.assertEqual(a, b)
 
 if __name__ == '__main__':
     unittest.main()
