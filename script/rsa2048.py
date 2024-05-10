ed2fafdfaabbaab8929a1869b7ad024ea59be0f7
Yoda Lee <lc85301@gmail.com>
2024-05-10 22:56:08 +0800

    Implement ntt for length 4 list

--- script/rsa2048.py
+++ script/rsa2048.py
@@ -1,11 +1,13 @@
 import typing
 
+
 def intmul_2048(a, b, n):
     """Calculate a * b % n with a, b, n are 2048 bits integer"""
-    assert(a < 2**2048)
-    assert(b < 2**2048)
-    assert(n < 2**2048)
+    assert (a < 2**2048)
+    assert (b < 2**2048)
+    assert (n < 2**2048)
     return (a * b) % n
+
 
 def chunk(a: int) -> list[int]:
     """For 32 bits operation l = 11, chunk number every 11 bits;
@@ -18,9 +20,50 @@ def chunk(a: int) -> list[int]:
         a >>= 11
     return xs
 
+
 def dechunk(xs: list[int]) -> int:
     a = 0
     for x in reversed(xs):
         a <<= 11
         a = a | x
     return a
+
+
+def CT_BFU(a, b, omega, q):
+    m = (b * omega) % q
+    return (a + m) % q, (a - m) % q
+
+
+def GS_BFU(a, b, omega, q):
+    add = (a + b) % q
+    sub = ((a - b) * omega) % q
+    add = add // 2 if add % 2 == 0 else (add + q) // 2
+    sub = sub // 2 if sub % 2 == 0 else (sub + q) // 2
+    return add, sub
+
+
+def br(i: int, l: int) -> int:
+    s = bin(i)[2:].zfill(l)
+    return int(s[::-1], 2)
+
+
+def ntt_4(xs: list[int], omega: int, prime: int) -> list[int]:
+    l = len(xs)
+    twiddle_factors = [pow(omega, i, prime) for i in range(l)]
+    twiddle_index = 1
+
+    distance = l//2
+
+    while distance >= 1:
+        for start in range(0, l, 2*distance):
+            for i in range(distance):
+                tf = twiddle_factors[br(twiddle_index, 2)]
+                idx1 = start + i
+                idx2 = start + i + distance
+                xs[idx1], xs[idx2] = CT_BFU(xs[idx1],
+                                            xs[idx2], tf, prime)
+            twiddle_index += 1
+
+        distance //= 2
+
+    return xs
--- script/test_rsa2048.py
+++ script/test_rsa2048.py
@@ -2,6 +2,7 @@ import unittest
 import random
 from rsa2048 import *
 
+
 class TestIntmul2048(unittest.TestCase):
     def test_intmul_2048(self):
         # Generate random 2048-bit integers a, b, and n
@@ -17,12 +18,23 @@ class TestIntmul2048(unittest.TestCase):
         result = intmul_2048(a, b, n)
 
         # Check if the result is equal to 1
-        self.assertEqual(result, (a * b) % n, "Result of intmul_2048 is incorrect")
+        self.assertEqual(result, (a * b) %
+                         n, "Result of intmul_2048 is incorrect")
 
     def test_chunk_dechunk(self):
         a = random.getrandbits(2048)
         b = dechunk(chunk(a))
         self.assertEqual(a, b)
+
+    def test_ntt(self):
+        l = [1, 2, 3, 4]
+        l = ntt_4(l, 1925, 7681)
+        self.assertEqual([1467, 3471, 2807, 7621], l)
+
+    def test_br(self):
+        self.assertEqual(br(1, 8), 128)
+        self.assertEqual(br(2, 8), 64)
+
 
 if __name__ == '__main__':
     unittest.main()
